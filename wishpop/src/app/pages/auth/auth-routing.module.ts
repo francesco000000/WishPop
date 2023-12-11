47457b146c1a8d00c117a90442516619d67a862f
andreacruciani9 <140162473+andreacruciani9@users.noreply.github.com>
2023-12-11 18:30:11 +0100

    spostato modulo in pages

--- wishpop/src/app/pages/auth/auth-routing.module.ts
+++ wishpop/src/app/pages/auth/auth-routing.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AuthComponent } from './auth.component';
 import { LoginComponent } from './login/login.component';
-import { HomeComponent } from '../../home/home.component';
+import { HomeComponent } from '../home/home.component';
 
 const routes: Routes = [
   { path: '', component: AuthComponent },
